1a64419c59eef91ff6c8359851d07eff76a68480
michal.ciszewski <michal.ciszewski@rainlabs.pl>
2015-09-07 15:57:11 +0200

    Advanced test for calculator
    -tests revealed errors in calculator

---
+++ NSrtm.Core.xTests/Spline2DCalculatorTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using NSrtm.Core.Pgm.BiCubicInterpolation;
+using Xunit;
+
+namespace NSrtm.Core.xTests
+{
+    public class Spline2DCalculatorTests
+    {
+        [Theory]
+        [MemberData("ExternalData")]
+        public void BicubicSplineCalculatorReturnSimilarResultToExternalData(List<List<double>> values, double step)
+        {
+            var spline = Spline2DCalculator.GetBiCubicSpline(values, step);
+            var result = spline(2.05, 2.2);
+            Assert.Equal(result, 29.5);
+        }
+
+        #region Static Members
+
+        public static IReadOnlyList<object[]> ExternalData
+        {
+            get
+            {
+                return new List<object[]>
+                       {
+                           new object[]
+                           {
+                               new List<List<double>>
+                               {
+                                   new List<double> {33, 33, 33, 33},
+                                   new List<double> {31, 30, 31, 32},
+                                   new List<double> {28, 29, 29, 30},
+                                   new List<double> {26, 27, 28, 29},
+                               },
+                               1
+                           }
+                       };
+            }
+        }
+
+        #endregion
+    }
+}
